103070f732fe4c34e5b7044f1b7124217ff04c7f
cypherdavy <166112437+cypherdavy@users.noreply.github.com>
2025-08-24 12:11:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "yumi = yumi.main:main"
+            "yumi = yumi.cli:main"
         ],
     },
     author="Davy Cipher",
