7d0fa450110b4b1aef1822ea8a0176b90d433654
cypherdavy <166112437+cypherdavy@users.noreply.github.com>
2025-08-24 11:03:34 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="yumi",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "beautifulsoup4",
+        "rich",
+        "pyfiglet"
+    ],
+    entry_points={
+        "console_scripts": [
+            "yumi = yumi.main:main"
+        ],
+    },
+    author="Your Name",
+    description="Yumi - JS Recon & P1 Bug Hunter",
+    url="https://github.com/yourgithub/yumi"
+)
 
